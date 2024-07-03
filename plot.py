25c9413659673a086018285821091c358433e66b
Achilleas Pappas <acmwl123@gmail.com>
2024-07-03 17:16:07 +0300

    plotter update

--- plot.py
+++ plot.py
@@ -1,19 +1,21 @@
 import matplotlib.pyplot as plt
 import numpy as np
-colors = ['b','g','r','c','m','y','k','w']
-
+colors = ['b','g','r','c','m','y','k']
+global rectSum
 
 def plotTrajectories(trajectoryFile):
+    delimiter = ","
     fd = open(trajectoryFile)
     lines = fd.readlines()
-    users = int(lines[0])
+    #users = int(lines[0])
+    users = 7
     counter = 1
     color = 0
     for i in range(users):
         pointsX = np.array([])
         pointsY = np.array([])
-        while(int(lines[counter].split(",")[0])==i):
-            arr = lines[counter].split(",")
+        while(int(lines[counter].split(delimiter)[0])==i):
+            arr = lines[counter].split(delimiter)
             pointsX= np.append(pointsX,float(arr[2]))
             pointsY = np.append(pointsY,float(arr[3]))
             counter+=1
@@ -21,19 +23,28 @@ def plotTrajectories(trajectoryFile):
                 break
         plt.plot(pointsX,pointsY,colors[color])
         color=(color+1)%len(colors)
+    #plt.xlim(0,2000)
+    #plt.ylim(0,2000)
+    #plt.savefig(trajectoryFile+".png")
     plt.show()
 
 def plotRectangles(rectFile):
+    delimiter = " "
+    rectSum = 0
     fd = open(rectFile)
     lines = fd.readlines()
-    users = int(lines[0])
+    #users = int(lines[0])
+    users = 1000
     counter = 1
     color = 0
     for i in range(users):
-        while(int(lines[counter].split(" ")[0])==i):
+        previous = int(lines[counter].split(delimiter)[0])
+        rectNum = 0
+        while(previous == int(lines[counter].split(delimiter)[0])):
+            previous = int(lines[counter].split(delimiter)[0])
             pointsX = np.array([])
             pointsY = np.array([])
-            arr = lines[counter].split(" ")
+            arr = lines[counter].split(delimiter)
             #start
             pointsX = np.append(pointsX,float(arr[5]))#xstart 0 1 2 3 4
             pointsY = np.append(pointsY,float(arr[6]))#ystart
@@ -50,11 +61,22 @@ def plotRectangles(rectFile):
             pointsX= np.append(pointsX,float(arr[5]))#xstart
             pointsY = np.append(pointsY,float(arr[6]))#ystart
             counter+=1
+            rectNum+=1
+            #plt.plot(pointsX,pointsY,colors[color])
             if(counter>=len(lines)):
                 break
-            plt.plot(pointsX,pointsY,colors[color])
         color=(color+1)%len(colors)
-    plt.show()
-    
+        print("User ",i," Rectangles: ",rectNum)
+        rectSum += rectNum
+    print("Average: " ,rectSum/users)
+    #plt.xlim(0,2000)
 
+    #plt.savefig(rectFile+".png",transparent=True)
+    #plt.show()
+
+
+
+#plotTrajectories("/home/lookos/CS/Thesis/Generated/users.csv")
+#plotTrajectories("/media/lookos/LUKUSB/ShoppingCenter/UserTrajectories.csv")
+plotTrajectories("/home/lookos/Dropbox/CustomTrajectoryGenerator/printable.csv")
 
